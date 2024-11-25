9882ee55812e7eff6879ee66b662e663c6e9f2d4
Marwa <marwafakhfakh700@gmail.com>
2024-11-25 16:14:09 +0100

    products/orders

---
+++ OrdersAPI/Orders.cs
@@ -0,0 +1,10 @@
+﻿namespace OrdersAPI
+{
+    public class Orders
+    {
+        public int OrdersId { get; set; }
+        public DateTime OrderDate { get; }
+        public string? CustomerName { get; set; }
+        public int ProductId { get; set; }
+    }
+}
